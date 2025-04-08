687f3be720e3907c4023593e53662efa01908db3
Yogeshwar B. <byogeshwarreddy@gmail.com>
2025-04-08 21:01:16 +0530

    Added window_size plugin and implemented basic light/dark theme based on system theme.

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:window_size/window_size.dart';
+
+final ThemeData lightTheme = ThemeData(
+  colorScheme: ColorScheme.light(
+    primary: Colors.grey[800]!,
+    secondary: Colors.grey[600]!,
+    surface: Colors.grey[300]!,
+    onPrimary: Colors.white,
+    onSecondary: Colors.black,
+    onSurface: Colors.black,
+  ),
+  textTheme: TextTheme(
+    headlineLarge: TextStyle(fontSize: 60, color: Colors.black),
+  ),
+);
+
+final ThemeData darkTheme = ThemeData(
+  colorScheme: ColorScheme.dark(
+    primary: Colors.grey[900]!,
+    secondary: Colors.grey[700]!,
+    surface: Colors.grey[800]!,
+    onPrimary: Colors.white,
+    onSecondary: Colors.white,
+    onSurface: Colors.white,
+  ),
+  textTheme: TextTheme(
+    headlineLarge: TextStyle(fontSize: 60, color: Colors.white),
+  ),
+);
+final appTitle = "Katit";
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  setWindowTitle(appTitle);
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Katit',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const Scaffold(
-        backgroundColor: Colors.black,
-        body: Center(
-          child: Text(
-            'Katit',
-            style: TextStyle(color: Colors.white, fontSize: 60),
-          ),
-        ),
+      title: 'Katit Title bar',
+      theme: lightTheme, // Light theme applied by default
+      darkTheme: darkTheme, // Dark theme configuration
+      themeMode:
+          ThemeMode.system, // Automatically switch based on system settings
+      home: Builder(
+        builder: (context) {
+          return Scaffold(
+            backgroundColor:
+                Theme.of(context).colorScheme.surface, // Should be grey[800]
+            body: Center(
+              child: Text(
+                'Katit',
+                style: Theme.of(context).textTheme.headlineLarge,
+              ),
+            ),
+          );
+        },
       ),
     );
   }
